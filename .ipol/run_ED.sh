48a590679c9c1e07aa61ed689d659ec7a061e1df
Pascal Monasse <pascal.monasse@enpc.fr>
2026-04-10 10:07:46 +0200

    Demo ability to fix log10(eps) and subline validation

--- .ipol/run_ED.sh
+++ .ipol/run_ED.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# Arguments: scales (>=0)
+# Arguments:
 # -gradient: min gradient 
 # -gap: anchor gap
 # -length: min length
 # -sigma: Gaussian blur
-# -valid: a conrario validation
+# -valid: a conrario validation (true/false)
+# -leps: log10(epsilon) validation threshold (optional)
+# -sub: sublines (true/false, optional)
 
 grad=$1
 gap=$2
 length=$3
 sigma=$4
-valid=$5
-valid=${valid/true/-e 0}
-valid=${valid/false/}
+
+if [ "$5" = "true" ]; then
+   valid="-e ${6:-0}" # default log10(eps)=0
+   if [ "$7" = "true" ]; then
+      valid="$valid -S";
+   fi
+fi
 
 $bin/build/edgeDrawing $valid -g $grad -a $gap -l $length -s $sigma $input_0 edges.png
